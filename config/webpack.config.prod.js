1b3f6d9a402ef58c0a5471f5b210cebb28ed8cd7
shunji zhan <shunji.zhan@procore.com>
2017-03-03 13:14:39 -0800

    font awesome works locally but not after publish

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -16,29 +16,36 @@ module.exports = {
       },
 
       {
-        test: /\.(woff|svg|eot|ttf|woff2)$/,
-        loader: 'file-loader',
-        query: {
-          name: '[sha512:hash:base64:7].[ext]',
-        },
-      },
-
-      {
         // test: /\.s?[ca]ss$/,
         test: /\.css$/,
         exclude: /node_modules/,
         loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' 
       },
 
-      { 
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-        loader: "url-loader?limit=10000&mimetype=application/font-woff" 
-      },
+      // {
+      //   test: /\.(woff|svg|eot|ttf|woff2)$/,
+      //   loader: 'file-loader',
+      //   // loader: 'file?name=public/fonts/[name].[ext]',
+      //   query: {
+      //     name: '[sha512:hash:base64:7].[ext]',
+      //   },
+      // },
+
+      // { test: /\.svg$/, loader: 'url?limit=65000&mimetype=image/svg+xml&name=public/fonts/[name].[ext]' },
+      // { test: /\.woff$/, loader: 'url?limit=65000&mimetype=application/font-woff&name=public/fonts/[name].[ext]' },
+      // { test: /\.woff2$/, loader: 'url?limit=65000&mimetype=application/font-woff2&name=public/fonts/[name].[ext]' },
+      // { test: /\.[ot]tf$/, loader: 'url?limit=65000&mimetype=application/octet-stream&name=public/fonts/[name].[ext]' },
+      // { test: /\.eot$/, loader: 'url?limit=65000&mimetype=application/vnd.ms-fontobject&name=public/fonts/[name].[ext]' },
 
       // { 
-      //   test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+      //   test: /\.(woff|svg|eot|ttf|woff2)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
       //   loader: "file-loader" 
       // },
+
+      { test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=100000' }, 
+
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
 
       // {
       //   loader: 'autoprefixer-loader',
