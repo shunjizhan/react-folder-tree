b95402f602d3b10bc2c1252ff948d774653ac1cb
shunji zhan <shunji.zhan@procore.com>
2017-03-10 09:45:30 -0800

    changed EditableName class to state class

--- src/Components/EditableName.js
+++ src/Components/EditableName.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import styles from './folderTreeCSS.css'
 
-const EditableName = ({filename, setMyName}) => (
-	<span>
-
-    {' ' + filename + ' '}
-    <i className={styles.pencilIcon} onClick={() => { setMyName('*' + filename + '*') }} />
-
-  </span>
-);
-
-EditableName.propTypes = {
+class EditableName extends React.Component {
+	static propTypes = {
   filename: React.PropTypes.string.isRequired,
   setMyName: React.PropTypes.func.isRequired,
 };
+
+	constructor(props) {
+    super(props);
+
+    this.state = {
+    	children: props.children,
+    	level: props.level,
+      open: false,
+    };
+  }
+
+	render() { 
+		return (	
+			<span>
+
+    		{' ' + this.props.filename + ' '}
+    		<i className={styles.pencilIcon} onClick={() => { this.props.setMyName('*' + this.props.filename + '*') }} />
+
+  		</span>
+		);
+	}
+
+}
+
 
 export default EditableName;
