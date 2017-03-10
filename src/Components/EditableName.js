1ed83b503ceef296ca763e91a33e9a1a330dfc3f
shunji zhan <shunji.zhan@procore.com>
2017-03-10 10:24:03 -0800

    ok and no button works

--- src/Components/EditableName.js
+++ src/Components/EditableName.js
@@ -19,27 +19,37 @@ class EditableName extends React.Component {
 
   toggleEditing() {
   	this.setState(prevState => ({editing: !prevState.editing}));
+  	if (this.state.editing)
+  		this.textInput.focus();
   }
 
   handleChangeName(e) {
-  	this.props.setMyName(e.target.value);
+  	this.props.setMyName(this.textInput.value);
+  	this.toggleEditing();
   }
 
 	render() { 
 		// this.props.setMyName('*' + this.props.filename + '*')
 		let input = (
 			<span>
-				<input type="text" defaultValue={this.props.filename} />
-				<i className={styles.OKIcon} />
-				<i className={styles.NoIcon} />
+				<input type="text" defaultValue={this.props.filename} ref={ input => { this.textInput = input; } } />
+				<i className={styles.OKIcon} onClick={this.handleChangeName} />
+				<i className={styles.NoIcon} onClick={this.toggleEditing} />
+			</span>
+		);
+
+		let name = (
+			<span>
+				{' ' + this.props.filename + ' '}
+				<i className={styles.pencilIcon} onClick={this.toggleEditing} />
 			</span>
 		);
 
 		return (	
 			<span>
 
-    		{this.state.editing? input : ' ' + this.props.filename + ' '}
-    		<i className={styles.pencilIcon} onClick={this.toggleEditing} />
+    		{ this.state.editing? input : name }
+
 
   		</span>
 		);
