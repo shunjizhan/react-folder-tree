04f5020283446895072017aa23cbd71d83bfdf45
Shunji Zhan <shunhahaha@live.com>
2020-11-08 14:39:38 -0800

    Set Up Husky Pre-commit Pipeline
    - installed husky and lint-staged package
    - added and configured more eslint rules
    - fixed some exlint errors

--- src/components/SandBox.jsx
+++ src/components/SandBox.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
 
 import FolderTree from './FolderTree/FolderTree';
 import testData from '../utils/testData';
