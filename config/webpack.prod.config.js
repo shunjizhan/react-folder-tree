33ca00de201ff9a78a6ce67ae2fb415183acbb64
shunji <shunhahaha@live.com>
2021-01-31 00:29:49 +0800

    prod webpack is now in production more, and is able to treeshaking react-icons library

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const CleanTerminalPlugin = require('clean-terminal-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 // TODO: code split the react icons library
 
 module.exports = {
-  mode: 'development',  // TODO: change to prod when finished
-  devtool: 'inline-source-map',
+  mode: 'production',
   entry: path.resolve(__dirname, '../src/components/FolderTree/FolderTree.jsx'),
   output: {
     path: path.resolve(__dirname, '../dist'),
@@ -15,7 +15,7 @@ module.exports = {
   },
   resolve: {
     // our code can resolve 'xxx' instead of writing 'xxx.jsx'
-    extensions: ['*', '.js', '.jsx'],
+    extensions: ['*', '.js', '.jsx', '.mjs'],
   },
   module: {
     // For every file that match regex in 'test', webpack pipes the code through to loaders
@@ -40,6 +40,7 @@ module.exports = {
   plugins: [
     // clear terminal in each build
     new CleanTerminalPlugin(),
+    // new BundleAnalyzerPlugin(),
   ],
   externals: {
     // this is needed for react to resolve to a single react
